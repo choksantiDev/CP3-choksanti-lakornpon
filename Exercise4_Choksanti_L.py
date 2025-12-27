f8368b247dd9cad1609e7da2cfb6ecfade751230
choksantiDev <choksanti25@gmail.com>
2025-12-27 08:24:00 +0700

    Create Exercise4_Choksanti_L.py

---
+++ Exercise4_Choksanti_L.py
@@ -0,0 +1,22 @@
+"""
+--- Your Score ---
+
+Foundation English : คะแนนที่ได้
+
+General Business : คะแนนที่ได้
+
+Introduction to Computer Systems : คะแนนที่ได้
+
+Computer Programming : คะแนนที่ได้
+"""
+
+found_eng_score = float(input("Foundation English :"))
+gen_bu_score = float(input("General Business :"))
+intro_com_score = float(input("Introduction to Computer Systems :"))
+com_pro_score = float(input("Computer Programming :"))
+
+print("--- Your Score ---")
+print("Foundation English :", found_eng_score)
+print("General Business :", gen_bu_score)
+print("Introduction to Computer Systems :", intro_com_score)
+print("Computer Programming :", com_pro_score)
